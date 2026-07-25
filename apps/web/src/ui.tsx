452a745dd3aa52e9d0102e4e5531bd51c8383f14
big-dudu-mosty <2729246636@qq.com>
2026-07-25 09:33:55 +0800

    Add project logo as favicon and nav brand image

--- apps/web/src/ui.tsx
+++ apps/web/src/ui.tsx
@@ -82,6 +82,7 @@ export function Nav() {
   return (
     <nav className="nav">
       <NavLink to="/" className="brand" onClick={() => setOpen(false)}>
+        <img src="/logo.png" alt="logo" style={{ height: 28, width: 'auto', imageRendering: 'pixelated', marginRight: 6, verticalAlign: 'middle' }} />
         {t('app.title')}
         <small>CATCH THE HOTSPOT</small>
       </NavLink>
